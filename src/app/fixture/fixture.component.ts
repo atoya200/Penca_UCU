55019ec910d8266e22d6dd65d1f4ae90be0824c7
Br1 <bruno.bugarin@correo.ucu.edu.uy>
2024-04-29 17:37:45 -0300

    Se creo el componente de Fixture

---
+++ src/app/fixture/fixture.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixture',
+  standalone: true,
+  imports: [],
+  templateUrl: './fixture.component.html',
+  styleUrl: './fixture.component.css'
+})
+export class FixtureComponent {
+
+}
