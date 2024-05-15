ecc7fa5918acfad066f21709a7b5eb85606e53a5
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-05-15 19:19:10 -0300

    Agregando comentarios al menu

--- Frontend/src/app/menu-principal/menu-principal.component.ts
+++ Frontend/src/app/menu-principal/menu-principal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LoginService } from '../login.service';
 @Component({
   selector: 'app-menu-principal',
@@ -9,17 +9,27 @@ import { LoginService } from '../login.service';
 })
 export class MenuPrincipalComponent {
 
+  @Input() isAdmin?: boolean;
+
   constructor(private loginService: LoginService) { }
 
-  send(): void {
-    this.loginService.test().subscribe(
-      data => {
-        console.log(data)
-      },
-      error => {
+  ngOnInit(): void{
+    // verificar si el usuario tiene permisos, sino mandarlo al login
+    // buscar las notificaciones que tiene el usuario (predicciones faltantes, etc)
+    // buscar campeonatos que esta inscripto el usuario, o si es administrador que muestre los campeonatos activos listados
+    
 
-        console.log(error);
-      });
+
+
   }
+
+  // TODO: 
+  // - agregar carrousel con imagenes de futbol en el menu principal
+  // - listar campeonatos en el inicio, al entrar al campeonato que aparezcan las funcionalidades que dependen de el
+  // - navbar con las funcionalidades de anotarse a un nuevo campeonato, ver notificaciones, 
+  // Equipos (ingresar equipos, modificar equipos, consultar equipos), 
+  
+
+
 
 }
