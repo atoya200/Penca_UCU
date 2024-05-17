5b231e3f895ea83e723939e74b88fa13f2f50f81
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-05-17 18:10:04 -0300

    Agregando campeonatos al menu, agregando endpoints del campeonato

--- Frontend/src/app/menu-principal/menu-principal.component.ts
+++ Frontend/src/app/menu-principal/menu-principal.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { LoginService } from '../login.service';
+import { ChampionshipService } from '../championship.service';
+import { NgIf, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-menu-principal',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, CommonModule],
   templateUrl: './menu-principal.component.html',
   styleUrl: './menu-principal.component.css'
 })
@@ -11,15 +15,23 @@ export class MenuPrincipalComponent {
 
   @Input() isAdmin?: boolean;
 
-  constructor(private loginService: LoginService) { }
+  Championships: any;
+  constructor(private loginService: LoginService, private championshipService: ChampionshipService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     // verificar si el usuario tiene permisos, sino mandarlo al login
     // buscar las notificaciones que tiene el usuario (predicciones faltantes, etc)
     // buscar campeonatos que esta inscripto el usuario, o si es administrador que muestre los campeonatos activos listados
-    
-
-
+    // Empezar un spinner y despues terminarlo?
+    this.championshipService.getChampionships().subscribe(
+      data => {
+        console.log(data)
+        this.Championships = data.championships
+      },
+      error => {
+        alert(error.error.msg)
+        console.log(error);
+      });
 
   }
 
@@ -28,7 +40,7 @@ export class MenuPrincipalComponent {
   // - listar campeonatos en el inicio, al entrar al campeonato que aparezcan las funcionalidades que dependen de el
   // - navbar con las funcionalidades de anotarse a un nuevo campeonato, ver notificaciones, 
   // Equipos (ingresar equipos, modificar equipos, consultar equipos), 
-  
+
 
 
 
