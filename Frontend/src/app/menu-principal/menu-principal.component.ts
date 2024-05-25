9333fb107e4fcd25ca1ac44774f6871a9b6b005e
Br1 <bruno.bugarin@correo.ucu.edu.uy>
2024-05-25 16:56:57 -0300

    Merge

--- Frontend/src/app/menu-principal/menu-principal.component.ts
+++ Frontend/src/app/menu-principal/menu-principal.component.ts
@@ -178,7 +178,7 @@ export class MenuPrincipalComponent implements AfterViewInit {
   elegirEquipos(): void {
     // validar si el ID penca(campeonato) existe
     // Si existe, ir a buscar los equiops del campeonato y cargarlos en el select
-
+    /*
     this.championshipService.getTeams(this.angFormPencaId.get('idPenca').value).subscribe(
       data => {
         this.Equipos = data.teams;
@@ -197,7 +197,7 @@ export class MenuPrincipalComponent implements AfterViewInit {
         alert(error.error.msg)
         console.log(error);
       });
-
+      */
 
   }
 
