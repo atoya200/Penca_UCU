2f146f0805480338efdebb3f40278f5c94e02028
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-06-09 20:11:01 -0300

    Agregando estadisticas

--- Frontend/src/app/statistics.service.spec.ts
+++ Frontend/src/app/statistics.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ChampionshipService } from './championship.service';
+import { StatisticsService } from './statistics.service';
 
-describe('ChampionshipService', () => {
-  let service: ChampionshipService;
+describe('StatisticsService', () => {
+  let service: StatisticsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ChampionshipService);
+    service = TestBed.inject(StatisticsService);
   });
 
   it('should be created', () => {
